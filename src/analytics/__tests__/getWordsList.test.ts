21593cb6e470a16a3b4f7e1f9ceb78e50587606d
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-05 20:24:56 +0100

    Define getWordsList tests

---
+++ src/analytics/__tests__/getWordsList.test.ts
@@ -0,0 +1,100 @@
+import { getWordsList } from "../index.js";
+import { logTestResult } from "./logTestResult.js";
+
+const test1 = (): void => {
+  const words = ["hello", "world"];
+  const expectedList = "hello, world";
+
+  const wordsList = getWordsList(words);
+
+  console.log(`\n${getWordsList.name}()`);
+
+  console.log(`Test 1: "${words}"`);
+  console.log(`Expected Result: "${expectedList}" | Result: "${wordsList}"`);
+
+  if (expectedList === wordsList) {
+    console.log("Test OK ✅");
+  } else {
+    console.log("Test failed ❌");
+  }
+};
+
+const test2 = (): void => {
+  const words = ["", ""];
+  const expectedList = "";
+
+  const wordsList = getWordsList(words);
+
+  console.log(`\n${getWordsList.name}()`);
+
+  console.log(`Test 2: "${words}"`);
+  console.log(`Expected Result: "${expectedList}" | Result: "${wordsList}"`);
+
+  if (expectedList === wordsList) {
+    console.log("Test OK ✅");
+  } else {
+    console.log("Test failed ❌");
+  }
+};
+
+const test3 = (): void => {
+  const words = ["\n", "\n"];
+  const expectedList = "";
+
+  const wordsList = getWordsList(words);
+
+  console.log(`\n${getWordsList.name}()`);
+
+  console.log(`Test 3: "${words}"`);
+  console.log(`Expected Result: "${expectedList}" | Result: "${wordsList}"`);
+
+  if (expectedList === wordsList) {
+    console.log("Test OK ✅");
+  } else {
+    console.log("Test failed ❌");
+  }
+};
+
+const test4 = (): void => {
+  const words = ["Hello"];
+  const expectedList = "Hello";
+
+  const wordsList = getWordsList(words);
+
+  console.log(`\n${getWordsList.name}()`);
+
+  console.log(`Test 4: "${words}"`);
+  console.log(`Expected Result: "${expectedList}" | Result: "${wordsList}"`);
+
+  if (expectedList === wordsList) {
+    console.log("Test OK ✅");
+  } else {
+    console.log("Test failed ❌");
+  }
+};
+
+const test5 = () => {
+  const words: string[] = [];
+  const expectedList = "";
+
+  const wordsList = getWordsList(words);
+
+  console.log(`\n${getWordsList.name}()`);
+
+  console.log(`Test 5: "${words}"`);
+  console.log(`Expected Result: "${expectedList}" | Result: "${wordsList}"`);
+
+  if (expectedList === wordsList) {
+    console.log("Test OK ✅");
+  } else {
+    console.log("Test failed ❌");
+  }
+};
+
+export const getWordsListTest = (): void => {
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+};
--- src/analytics/__tests__/index.ts
+++ src/analytics/__tests__/index.ts
@@ -1,9 +1,11 @@
 import { getCharactersTotalTest } from "./getCharactersTotal.test.js";
 import { getParagraphsTotalTest } from "./getParagraphsTotal.test.js";
 import { getShortWordsTotalTest } from "./getShortWordsTotal.test.js";
+import { getWordsListTest } from "./getWordsList.test.js";
 import { getWordsTotalTest } from "./getWordsTotal.test.js";
 
 getParagraphsTotalTest();
 getWordsTotalTest();
 getCharactersTotalTest();
 getShortWordsTotalTest();
+getWordsListTest();
