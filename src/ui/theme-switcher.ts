5b00b3e5eee15f5ff70914e98c7817710b1ac9aa
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-06 20:46:49 +0100

    Add dark mode styles

--- src/ui/theme-switcher.ts
+++ src/ui/theme-switcher.ts
@@ -2,8 +2,8 @@ const switcher = document.querySelector(".switch input") as HTMLInputElement;
 
 switcher.addEventListener("change", () => {
   if (switcher.checked) {
-    document.querySelector(".container")?.classList.add("dark");
+    document.querySelector("body")?.classList.add("dark");
   } else {
-    document.querySelector(".container")?.classList.remove("dark");
+    document.querySelector("body")?.classList.remove("dark");
   }
 });
